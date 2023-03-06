d673954860756edb9951891c287833d0fccde54c
Heidar Kariman <karimanhsz@gmil.com>
2023-03-06 15:08:56 +0000

    init framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToolsTableComponent } from './tools-table/tools-table.component';
 import { ToolsDiagramComponent } from './tools-diagram/tools-diagram.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'tools-table', component: ToolsTableComponent },
-  { path: 'tools-diagram', component: ToolsDiagramComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'toolstable', component: ToolsTableComponent },
+  { path: 'toolsdiagram', component: ToolsDiagramComponent },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
