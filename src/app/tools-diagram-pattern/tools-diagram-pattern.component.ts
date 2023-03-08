f4ab27c7800d4829c420310945e4c524613553c8
Heidar Kariman <karimanhsz@gmil.com>
2023-03-08 11:09:01 +0000

    last version

---
+++ src/app/tools-diagram-pattern/tools-diagram-pattern.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tools-diagram-pattern',
+  templateUrl: './tools-diagram-pattern.component.html',
+  styleUrls: ['./tools-diagram-pattern.component.css']
+})
+export class ToolsDiagramPatternComponent {
+
+}
